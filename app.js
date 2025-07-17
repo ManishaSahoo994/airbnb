eea3f1101950616c82444ef9b2fa2b20e055deba
Manisha Sahoo <manishasahoo2343@gmail.com>
2025-07-17 10:16:38 +0530

    View page added completed

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //external module
 const express=require("express");
+//core module
+const path = require("path");
 //local module
 const userRouter = require("./routes/userRouter");
 const hostRouter = require("./routes/hostRouter");
@@ -17,7 +19,7 @@ app.use(userRouter);
 app.use("/host",hostRouter);
 
 app.use((req,res,next)=>{
-    res.status(404).send(`<h1>Page not found.</h1>`);
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 
