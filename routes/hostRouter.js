cd69caa022df46c922f1632fd19c7054a62490a9
Manisha Sahoo <manishasahoo2343@gmail.com>
2025-07-18 09:33:55 +0530

    Adding path file to other files

--- routes/hostRouter.js
+++ routes/hostRouter.js
@@ -4,13 +4,14 @@ const path = require("path");
 //external module
 const express= require("express");
 const hostRouter = express.Router();
+const rootDir = require("../utils/pathUtil");
 
 hostRouter.get("/add-home",(req,res,next)=>{
-       res.sendFile(path.join(__dirname, '../', 'views', 'addHome.html'));
+       res.sendFile(path.join(rootDir, 'views', 'addHome.html'));
 });
 
 hostRouter.post("/add-home",(req,res,next)=>{
-       res.sendFile(path.join(__dirname, '../', 'views', 'homeAdded.html'));
+       res.sendFile(path.join(rootDir, 'views', 'homeAdded.html'));
 });
 
 module.exports = hostRouter;
