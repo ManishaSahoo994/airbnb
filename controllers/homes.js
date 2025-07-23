eac15cfa943875c321bc7400df78af975c0fe29f
Manisha Sahoo <manishasahoo2343@gmail.com>
2025-07-23 19:56:45 +0530

    Model added successfully in MVC

--- controllers/homes.js
+++ controllers/homes.js
@@ -1,16 +1,26 @@
+const Home = require("../models/home");
+
 exports.getAddHome = (req,res,next)=>{
        res.render('addHome',{pageTitle: 'Add Home to airbnb', currentPage: 'addHome'});
 };
 
-const registeredHomes =[];
 
 exports.postAddHome=(req,res,next)=>{
        console.log('Home Registration successful for:', req.body);
-       registeredHomes.push(req.body);
+         
+       //there is repetation of req.body so do not use it 
+       //const home = new Home(req.body.houseName, req.body.price, req.body.location, req.body.rating, req.body.photoUrl);
+       
+       //use this better approach
+       const {houseName, price, location, rating, photoUrl} = req.body;
+       const home = new Home(houseName, price, location, rating, photoUrl);
+       home.save();
+
        res.render('homeAdded',{pageTitle: 'Home Added Successfully', currentPage: 'homeAdded'});
 };
 
 exports.getHomes = (req,res,next)=>{
+       const registeredHomes = Home.fetchAll();
        console.log(registeredHomes);
        res.render('home',{registeredHomes : registeredHomes, pageTitle : 'airbnb home', currentPage: 'Home'});
 };
