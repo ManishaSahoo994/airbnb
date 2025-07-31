0f8d6ebe1ce7cbdb8f6912f0ef3c2a08f6e60ebb
Manisha Sahoo <manishasahoo2343@gmail.com>
2025-07-31 09:39:01 +0530

    Edit home page with proper path allocated for host completed

--- controllers/hostController.js
+++ controllers/hostController.js
@@ -1,7 +1,13 @@
 const Home = require("../models/home");
 
 exports.getAddHome = (req,res,next)=>{
-       res.render('host/addHome',{pageTitle: 'Add Home to airbnb', currentPage: 'addHome'});
+       res.render('host/edit-home',{pageTitle: 'Add Home to airbnb', currentPage: 'addHome'});
+};
+exports.getEditHome = (req,res,next)=>{
+       const homeId = req.params.homeId;
+       const editing = req.query.editing === 'true';
+       console.log(homeId, editing);
+       res.render('host/edit-home',{pageTitle: 'Edit your Home', currentPage: 'host-home', editing : editing});
 };
 
 exports.getHostHomes = (req,res,next)=>{
